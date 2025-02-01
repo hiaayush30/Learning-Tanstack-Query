e2edbf4a35bbe60d4fa44f5b182c469372b66fe6
aayush <hiaayush30@gmail.com>
2025-02-02 02:57:02 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
-import React from 'react'
-import { useQuery, UseQuery } from '@tanstack/react-query'
-import { getPosts } from './utils/api'
+import React, { FormEvent, FormEventHandler, useEffect, useRef } from 'react'
+import { useQuery, useMutation,useQueryClient} from '@tanstack/react-query'
+import { addPost, getPosts } from './utils/api'
 const App = () => {
-  const getPostsQuery = useQuery<Array<{ id: string, title: string, views: number }>>({
+  const { data, isLoading, isError, refetch: refetchGetPosts } = useQuery<Array<{ id: string, title: string, views: number }>>({
     queryKey: ['getPosts'],  //used by it to map and cache data
-    queryFn: getPosts 
+    queryFn: getPosts
   })
-  const { data, isLoading, isError } = getPostsQuery;
+
+  const titleRef = useRef<HTMLInputElement>(null);
+
+  const { mutate: addPostMutation, isSuccess: isAddPostSuccess, isPending: isAddPostPending } = useMutation({
+    mutationFn: addPost,
+    // onSuccess:refetchGetPosts
+    // onSuccess:()=>queryClient.invalidateQueries({queryKey:['getPosts']})
+  })
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const id = String(Math.random() * 10);
+    const title = titleRef.current.value;
+    const views = 500;
+    addPostMutation({ id, title, views });
+    refetchGetPosts();
+    titleRef.current.value = '';
+  }
+
+  const queryClient=useQueryClient();
+  useEffect(() => {
+    if (isAddPostSuccess) {
+      // refetchGetPosts();
+      //or
+      // queryClient.invalidateQueries({queryKey:['getPosts']})
+    }
+  }, [isAddPostSuccess,refetchGetPosts,queryClient])
   return (
     <div>
       Hello There
+      <h1>Add a new post</h1>
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='title' ref={titleRef} />
+        <button type='submit' disabled={isAddPostPending}>Add</button>
+      </form>
       {isLoading && <div>Loading...</div>}
       {!isLoading && !isError && data?.map(post => {
         return <div key={post.id}>
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,24 @@
-export const getPosts=async()=>{
-    const res =await fetch('http://localhost:3000/posts');
+export const getPosts = async () => {
+    const res = await fetch('http://localhost:3000/posts');
     return await res.json();
 };
-export const getError=async()=>{
+
+
+interface AddPost{
+    title:string;
+    id:string;
+    views:number;
+}
+export const addPost = async (body:AddPost) => {
+    const res = await fetch('http://localhost:3000/posts', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers:{
+            'Content-Type':'application/json; charset=UTF-8'
+        }
+    });
+    return await res.json();
+}
+export const getError = async () => {
     return Promise.reject('Error')
 };
